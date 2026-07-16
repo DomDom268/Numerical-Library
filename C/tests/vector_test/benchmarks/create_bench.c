73e2508ba0ea0ad1836cc452f3f1a7aa720f4c93
Dominc Christopher <christopherdominic268@gmail.com>
2026-07-16 00:19:57 -0500

    Added first benchmark test for the create-v function

---
+++ C/tests/vector_test/benchmarks/create_bench.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "vector.h"
+
+double bench_create( int trials, int size){
+
+    double comprehensive_time = 0;
+    for(int i=0;i<trials;i++){
+
+        clock_t start = clock();
+
+        vector *v = create_v(size);
+        free_v(&v);
+
+        clock_t end = clock();
+
+        double trial_time = (end-start);
+        comprehensive_time += trial_time;
+    }
+
+    double avg_time_elapsed = comprehensive_time/ trials;
+    
+    return avg_time_elapsed;
+}
+
+int main(void){
+    
+    double bench_time = bench_create(1000,10);
+    printf("Avergae time for the create_v function with over 1000 trials with size 10 vectors", bench_time);
+    
+    return 0;
+}
---
+++ C/tests/vector_test/stress/test_random.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define UNITY_INCLUDE_DOUBLE
+#include "unity.h"
+#include "vector.h"
+
+void setUp(void)
+{
+
+}
+
+void tearDown(void) 
+{
+
+}
+
