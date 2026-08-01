5875deafe67eb2f5d5b294e8e322969015478e03
Dominc Christopher <christopherdominic268@gmail.com>
2026-07-31 19:19:04 -0500

    Added mathemtical property tests for scalar matrix multiplication

--- C/tests/matrix_test/mathematical/test_math.c
+++ C/tests/matrix_test/mathematical/test_math.c
@@ -204,7 +204,7 @@ void test_inverse(void){
 *Ensures the addition function abides by closure
 *Expected: A+B=C ,where A,B,C are all mxn matrices
 */
-void test_closure(void){
+void test_add_closure(void){
     matrix *A = create(3,3);
     matrix *B = create(3,3);
 
@@ -234,11 +234,283 @@ void test_add(void){
     test_add_commuitative();
     test_additive_identity();
     test_additive_inverse();
-    test_closure();
+    test_add_closure();
     test_inverse();
 }
 
+/*Associativity Law for Matrix Scalar Multiplication
+*Ensures matrix scalar multiplication abides by the associativity law
+*Expected: r(sA) = (rs)A , where r,s e R
+*/
+void test_scalar_associative(void){
+    matrix *A = create(3,3);
+    double r = 3.0;
+    double s = 4.0;
 
-   
+    double A_val = 5;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+        }
+    }
+
+    matrix *result1 = scalar_multiply(scalar_multiply(A,s),r);
+    matrix *result2 = scalar_multiply(A,(r*s));
+    TEST_ASSERT_NOT_NULL(result1);
+    TEST_ASSERT_NOT_NULL(result2);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][0],result2->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][1],result2->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][2],result2->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][0],result2->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][1],result2->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][2],result2->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][0],result2->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][1],result2->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][2],result2->data[2][2]);
+
+    free_matrix(&A);
+    free_matrix(&result1);
+    free_matrix(&result2);
+
     
+}
+
+/*Scalar Distributive Test:(Over Matrix Multiplication)
+*Ensures that scalar matrix mutiplication abides by the distributive law over matrix multiplication
+*Expected: r(AB)= rA(B) = A(rB)
+*/
+void test_distributive_matrix_multiplication(void){
+    matrix *A = create(3,3);
+    matrix *B = create(3,3);
+    double r = 4.0;
+
+    double A_val = 5;
+    double B_val = 7;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+            setVal(B,i,j,B_val++);
+        }
+    }
+
+    matrix *result1 = scalar_multiply(mat_multiply(A,B),r);
+    matrix *result2 = mat_multiply(scalar_multiply(A,r),B);
+    matrix *result3 = mat_multiply(A,scalar_multiply(B,r));
+    TEST_ASSERT_NOT_NULL(result1);
+    TEST_ASSERT_NOT_NULL(result2);
+    TEST_ASSERT_NOT_NULL(result3);
+
+    //Asserting test for result 1 and 2
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][0],result2->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][1],result2->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][2],result2->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][0],result2->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][1],result2->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][2],result2->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][0],result2->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][1],result2->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][2],result2->data[2][2]);
+
+    //Asserting test for result 2 and 3
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[0][0],result3->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[0][1],result3->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[0][2],result3->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[1][0],result3->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[1][1],result3->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[1][2],result3->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[2][0],result3->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[2][1],result3->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result2->data[2][2],result3->data[2][2]);
+
+    //Asserting test for result 1 and 3
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][0],result3->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][1],result3->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][2],result3->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][0],result3->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][1],result3->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][2],result3->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][0],result3->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][1],result3->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][2],result3->data[2][2]);
+
+    free_matrix(&A);
+    free_matrix(&B);
+    free_matrix(&result1);
+    free_matrix(&result2);
+    free_matrix(&result3);
+    
+}
+
+/* Scalar Distributive Test:(Over Scalar Addition)
+*Ensures that scalar matrix  multiplication abides by the distributive law over scalar addition
+*Expected: (r+s)A = rA + rS
+*/
+void test_distributive_scalar_addition(void){
+    matrix *A = create(3,3);
+    double r  = 3.0;
+    double s = 9.0;
+
+    double A_val = 5;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+        }
+    }
+
+    matrix *result1 = scalar_multiply(A,(r+s));
+    matrix *result2 = mat_add(scalar_multiply(A,s),scalar_multiply(A,r));
+    TEST_ASSERT_NOT_NULL(result1);
+    TEST_ASSERT_NOT_NULL(result2);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][0],result2->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][1],result2->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][2],result2->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][0],result2->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][1],result2->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][2],result2->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][0],result2->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][1],result2->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][2],result2->data[2][2]);
+
+    free_matrix(&A);
+    free_matrix(&result1);
+    free_matrix(&result2);
+
+}
+
+/*Scalar Distributive Test(Over Matrix Addition)
+*Ensures that the scalar matrix multiplication function abides by the distributive law over matrix addition
+*Expected: r(A+B) = rA+rB
+*/
+void test_distributive_matrix_addition(void){
+    matrix *A = create(3,3);
+    matrix *B = create(3,3);
+    double r = 9.0;
+
+    double A_val = 5;
+    double B_val = 7;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+            setVal(B,i,j,B_val++);
+        }
+    }
+
+    matrix *result1 = scalar_multiply(mat_add(A,B),r);
+    matrix *result2 = mat_add(scalar_multiply(A,r),scalar_multiply(B,r));
+    TEST_ASSERT_NOT_NULL(result1);
+    TEST_ASSERT_NOT_NULL(result2);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][0],result2->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][1],result2->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[0][2],result2->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][0],result2->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][1],result2->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[1][2],result2->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][0],result2->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][1],result2->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(result1->data[2][2],result2->data[2][2]);
+
+    free_matrix(&A);
+    free_matrix(&B);
+    free_matrix(&result1);
+    free_matrix(&result2);
+
+}
+
+
+/*Multiplicative Inverse Law test for matrix scalar multiplication
+*Ensures matrix scalar multiplication abides by the multiplicative inverse law
+*Expected AX0 = 0 
+*/
+void test_scalar_multiplicative_inverse(void){
+    matrix *A = create(3,3);
+    double multiplicative_inverse = 0.0;
+
+    double A_val = 5;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+        }
+    }
+
+    matrix *result = scalar_multiply(A,multiplicative_inverse);
+    TEST_ASSERT_NOT_NULL(result);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(0.0,result->data[2][2]);
+
+    free_matrix(&A);
+    free_matrix(&result);
+}
+
+/*Multiplicative Identity Law test for matrix scalar multiplication
+*Ensures matrix scalar multiplication abides by the multiplicative identity law
+*Expected: AX1 = A
+*/
+void test_scalar_multiplicative_identity(void){
+    matrix *A = create(3,3);
+    double multiplicative_identity = 1.0;
+
+    double A_val = 5;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+        }
+    }
+
+    matrix *result = scalar_multiply(A,multiplicative_identity);
+    TEST_ASSERT_NOT_NULL(result);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[0][0],result->data[0][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[0][1],result->data[0][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[0][2],result->data[0][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[1][0],result->data[1][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[1][1],result->data[1][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[1][2],result->data[1][2]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[2][0],result->data[2][0]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[2][1],result->data[2][1]);
+    TEST_ASSERT_EQUAL_DOUBLE(A->data[2][2],result->data[2][2]);
+
+    free_matrix(&A);
+    free_matrix(&result);
+}
+
+/*Closure test for scalar matrix multiplication
+*Ensures that scalar matrix multiplication abides by the closure law
+*Expected: A = rB , B is mxn if A is mxn
+*/
+void test_scalar_closure(void){
+    matrix *A = create(3,3);
+    double r = 3.0;
+
+    double A_val = 5;
+    for(int i=0;i<A->cols;i++){
+        for(int j=0;j<A->cols;j++){
+            setVal(A,i,j,A_val++);
+        }
+    }
+
+    matrix *result = scalar_multiply(A,r);
+    TEST_ASSERT_NOT_NULL(result);
+    TEST_ASSERT_EQUAL_DOUBLE(3,result->rows);
+    TEST_ASSERT_EQUAL_DOUBLE(3,result->cols);
+
+    free_matrix(&A);
+    free_matrix(&result);
+}
+
+/*Comprehensive test for scalar matrix multiplication*/
+void test_scalar_comprehensive(void){
+    test_scalar_associative();
+    test_distributive_matrix_multiplication();
+    test_distributive_scalar_addition();
+    test_distributive_matrix_addition();
+    test_scalar_multiplicative_inverse();
+    test_scalar_multiplicative_identity();
+    test_scalar_closure();
+}
 
