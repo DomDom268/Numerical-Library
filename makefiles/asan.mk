262d7230cbcf811599391490a080d00a6673d78e
Dominc Christopher <christopherdominic268@gmail.com>
2026-08-12 18:52:39 -0500

    Added back asan enabled benchmark compilation

--- makefiles/asan.mk
+++ makefiles/asan.mk
@@ -14,7 +14,18 @@ EDGE_TESTS = C/tests/vector_test/edge/test_edge.c C/tests/vector_test/edge/test_
 
 MATH_TESTS = C/tests/vector_test/mathematical/test_math.c C/tests/vector_test/mathematical/test_math_runner.c
 
-# BENCH_TESTS = C/tests/vector_test/benchmark/test_benchmark.c C/tests/vector_test/benchmark/test_benchmark_runner.c
+BENCH_TESTS = C/tests/vector_test/benchmarks/add_bench.c \
+C/tests/vector_test/benchmarks/comprehensive_bench.c \
+C/tests/vector_test/benchmarks/create_bench.c \
+C/tests/vector_test/benchmarks/dot_bench.c \
+C/tests/vector_test/benchmarks/eucdist_bench.c \
+C/tests/vector_test/benchmarks/eucnorm_bench.c \
+C/tests/vector_test/benchmarks/mannorm_bench.c \
+C/tests/vector_test/benchmarks/normdist_bench.c \
+C/tests/vector_test/benchmarks/print_bench.c \
+C/tests/vector_test/benchmarks/scale_bench.c \
+C/tests/vector_test/benchmarks/setval_bench.c \
+C/tests/vector_test/benchmarks/sub_bench.c \
 
 
 #Build each test suite with ASAN enabled
@@ -27,11 +38,12 @@ asan_edge:
 asan_math:
 	$(CC) $(CFLAGS) $(ASAN_FLAGS) $(SRC) $(MATH_TESTS) -o asan_math -lm
 
-# asan_bench:
-# 	$(CC) $(CFLAGS) $(ASAN_FLAGS) $(SRC) $(BENCH_TESTS) -o asan_bench -lm
+asan_bench:
+	$(CC) $(CFLAGS) $(ASAN_FLAGS) $(SRC) $(BENCH_TESTS) -o asan_bench -lm
 
 #Run all test suites with ASAN enabled
-asan_all: asan_basic asan_edge asan_math 
+asan_all: asan_basic asan_edge asan_math asan_bench
 	./asan_basic
 	./asan_edge
 	./asan_math
+	./asan_bench
