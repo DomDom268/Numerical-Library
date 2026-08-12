28d03d09b8cfa8487782f85e666c0186d345860d
Dominc Christopher <christopherdominic268@gmail.com>
2026-08-12 18:58:30 -0500

    Continuation of previous commit

--- makefiles/asan.mk
+++ makefiles/asan.mk
@@ -5,7 +5,7 @@ CFLAGS = -IC/include -IC/Unity/src -DUNITY_INCLUDE_DOUBLE
 
 ASAN_FLAGS = -g -fsanitize=address -fno-omit-frame-pointer
 
-SRC = C/src/vector.c C/Unity/src/unity.c C/src/benchmark.c
+SRC = C/src/vector.c C/Unity/src/unity.c 
 
 #Test Suites
 BASIC_TESTS = C/tests/vector_test/basic/test_basic.c C/tests/vector_test/basic/test_basic_runner.c
@@ -14,18 +14,7 @@ EDGE_TESTS = C/tests/vector_test/edge/test_edge.c C/tests/vector_test/edge/test_
 
 MATH_TESTS = C/tests/vector_test/mathematical/test_math.c C/tests/vector_test/mathematical/test_math_runner.c
 
-BENCH_TESTS = C/tests/vector_test/benchmarks/add_bench.c \
-C/tests/vector_test/benchmarks/comprehensive_bench.c \
-C/tests/vector_test/benchmarks/create_bench.c \
-C/tests/vector_test/benchmarks/dot_bench.c \
-C/tests/vector_test/benchmarks/eucdist_bench.c \
-C/tests/vector_test/benchmarks/eucnorm_bench.c \
-C/tests/vector_test/benchmarks/mannorm_bench.c \
-C/tests/vector_test/benchmarks/normdist_bench.c \
-C/tests/vector_test/benchmarks/print_bench.c \
-C/tests/vector_test/benchmarks/scale_bench.c \
-C/tests/vector_test/benchmarks/setval_bench.c \
-C/tests/vector_test/benchmarks/sub_bench.c \
+BENCH_TESTS = C/src/benchmark.c
 
 
 #Build each test suite with ASAN enabled
