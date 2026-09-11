39cdd14a94a2f131612cb96194a915ebfb496978
Dominc Christopher <christopherdominic268@gmail.com>
2026-09-11 09:10:01 -0500

    Initial creation of benchmark module for matrix operations

---
+++ C/include/Matrix/benchmark.h
@@ -0,0 +1,12 @@
+/**
+ * @file Matrix/benchmark.c
+ * @brief This header file contains function declarations for benchmarking matrix module functions
+ */
+
+ #ifndef BENCHMARK_H
+ #define BENCHMARK_H
+
+ /**
+  * @brief Structure to store benchmark results
+  */
+ #endif // BENCHMARK_H
