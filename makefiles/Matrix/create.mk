5af8fa59a5357ccfc046e6be8e6e162f08589a33
Dominc Christopher <christopherdominic268@gmail.com>
2026-09-12 00:49:40 -0500

    Same as previous commit

--- makefiles/Matrix/create.mk
+++ makefiles/Matrix/create.mk
@@ -1,15 +1,15 @@
-CREATE_GC = gcc
+MCREATE_GC = gcc
 
-CREAT_CFLAGS += -IC/include
+MCREAT_CFLAGS += -IC/include
 
-CREATE_SRC = C/src/Matrix/matrix.c C/src/Matrix/benchmark.c
+MCREATE_SRC = C/src/Matrix/matrix.c C/src/Matrix/benchmark.c
 
-CREATE_TEST_SRC = C/tests/matrix_test/benchmarks/create_bench.c
+MCREATE_TEST_SRC = C/tests/matrix_test/benchmarks/create_bench.c
 
-CREATE_TARGET = mat_create
+MCREATE_TARGET = mat_create
 
 mat_create:
-	$(CREATE_GC) $(CREATE_CFLAGS) $(CREATE_SRC) $(CREATE_TEST_SRC) -o $(CREATE_TARGET)
+	$(MCREATE_GC) $(MCREATE_CFLAGS) $(MCREATE_SRC) $(MCREATE_TEST_SRC) -o $(MCREATE_TARGET)
 
 mat_create_run:
-	./$(CREATE_TARGET)
+	./$(MCREATE_TARGET)
--- makefiles/Matrix/sub.mk
+++ makefiles/Matrix/sub.mk
@@ -1,15 +1,15 @@
-SUB_GC = gcc
+MSUB_GC = gcc
 
-SUB_CFLAGS += -IC/include
+MSUB_CFLAGS += -IC/include
 
-SUB_SRC = C/src/Matrix/matrix.c C/src/Matrix/benchmark.c
+MSUB_SRC = C/src/Matrix/matrix.c C/src/Matrix/benchmark.c
 
-SUB_TEST_SRC = C/tests/matrix_test/benchmarks/sub_bench.c
+MSUB_TEST_SRC = C/tests/matrix_test/benchmarks/sub_bench.c
 
-SUB_TARGET = mat_sub
+MSUB_TARGET = mat_sub
 
 mat_sub:
-	$(SUB_GC) $(SUB_CFLAGS) $(SUB_SRC) $(SUB_TEST_SRC) -o $(SUB_TARGET)
+	$(MSUB_GC) $(MSUB_CFLAGS) $(MSUB_SRC) $(MSUB_TEST_SRC) -o $(MSUB_TARGET)
 
 mat_sub_run:
-	./$(SUB_TARGET)
+	./$(MSUB_TARGET)
